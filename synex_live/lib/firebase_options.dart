b159b45cd52b0be57db74c1b102f7b28a08ea2e0
filmexplainthefact-cloud <filmexplainthefact@gmail.com>
2026-04-08 16:12:18 +0530

    Update firebase_options.dart

--- synex_live/lib/firebase_options.dart
+++ synex_live/lib/firebase_options.dart
@@ -1,39 +1,26 @@
-// âš ï¸ Replace all values with your Firebase project credentials
-// Run: flutterfire configure  (recommended)
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    if (kIsWeb) return web;
+    if (kIsWeb) {
+      throw UnsupportedError('Web not configured');
+    }
     switch (defaultTargetPlatform) {
-      case TargetPlatform.android: return android;
-      case TargetPlatform.iOS: return ios;
-      default: throw UnsupportedError('Unsupported platform');
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError('iOS not configured');
+      default:
+        throw UnsupportedError('Unsupported platform');
     }
   }
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'YOUR_WEB_API_KEY',
-    appId: 'YOUR_WEB_APP_ID',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  );
+
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'YOUR_ANDROID_API_KEY',
-    appId: 'YOUR_ANDROID_APP_ID',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  );
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'YOUR_IOS_API_KEY',
-    appId: 'YOUR_IOS_APP_ID',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-    iosClientId: 'YOUR_IOS_CLIENT_ID',
-    iosBundleId: 'com.synex.synexLive',
+    apiKey: 'AIzaSyDjN72zb90c8GzasTiFx-wZHKk_XbL2XPw',
+    appId: '1:93914740482:android:976287a92168e91c1e741b',
+    messagingSenderId: '93914740482',
+    projectId: 'dgsell',
+    storageBucket: 'dgsell.firebasestorage.app',
   );
 }
